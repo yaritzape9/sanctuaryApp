5ca72b429df5cd9d2137afb0756c67363f0c965b
Yaritza Perez <yaritzape9@gmail.com>
2026-04-17 15:42:33 -0700

    adding footer content

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import Providers from "./providers";
 
@@ -32,8 +33,9 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <Providers>
           <Navbar />
-          {children}
+          <main>{children}</main>
         </Providers>
+        <Footer />
       </body>
     </html>
   );
